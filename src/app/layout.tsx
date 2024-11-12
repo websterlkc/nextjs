b0d6d71d203098a538ddcd8851f36591898266b9
webster-lou <webster.lou@yahoo.com>
2024-11-12 23:37:29 +0800

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { NotificationToaster } from "./shared/notification";
 import { Toaster } from 'sonner'
 
 const geistSans = localFont({
