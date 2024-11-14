f86c1d55879fd11c52d733741c1b806771fc4558
webster-lou <webster.lou@yahoo.com>
2024-11-14 21:50:23 +0800

    Fix unused variable

--- src/app/api/api-keys/validate/route.ts
+++ src/app/api/api-keys/validate/route.ts
@@ -1,5 +1,5 @@
 import { KeyController } from '../keyController';
-import { NextResponse, NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
   try {
