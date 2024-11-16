bcfd9c97320f73ce615fb281d72f4e54f4c03f56
webster-lou <webster.lou@yahoo.com>
2024-11-16 21:55:55 +0800

    Fix bug

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,6 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { AuthOptions } from "next-auth";
-
-export const authOptions: AuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-};
+import { authOptions } from "./auth";
 
 const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST }; 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import { authOptions } from "./api/auth/[...nextauth]/auth";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
